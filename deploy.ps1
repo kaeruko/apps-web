34622d8b8ea8035575acaa376e2759c5e92e4b3f
yokina <mail@cloxs.jp>
2026-08-18 17:31:46 +0900

    Merge main into ghost experience annotation

--- deploy.ps1
+++ deploy.ps1
@@ -1,16 +1,34 @@
 $ErrorActionPreference = "Stop"
 
+$distributionId = "E159EOKK4P9F3D"
+
 aws s3 sync .\public\ s3://ampere-apps-web/
 if ($LASTEXITCODE -ne 0) {
-    throw "S3 sync failed"
+    throw "S3 sync failed with exit code $LASTEXITCODE"
 }
 
-aws cloudfront create-invalidation `
-  --distribution-id E159EOKK4P9F3D `
-  --paths "/" "/index.html" "/garunavi/*" "/sitemap.xml"
+$invalidationId = aws cloudfront create-invalidation `
+    --distribution-id $distributionId `
+    --paths "/*" `
+    --query "Invalidation.Id" `
+    --output text
 
 if ($LASTEXITCODE -ne 0) {
-    throw "CloudFront invalidation failed"
+    throw "CloudFront invalidation creation failed with exit code $LASTEXITCODE"
+}
+
+if ([string]::IsNullOrWhiteSpace($invalidationId)) {
+    throw "CloudFront invalidation returned an empty ID"
+}
+
+Write-Host "Waiting for CloudFront invalidation: $invalidationId"
+
+aws cloudfront wait invalidation-completed `
+    --distribution-id $distributionId `
+    --id $invalidationId
+
+if ($LASTEXITCODE -ne 0) {
+    throw "CloudFront invalidation wait failed with exit code $LASTEXITCODE"
 }
 
 Write-Host "deploy done"
